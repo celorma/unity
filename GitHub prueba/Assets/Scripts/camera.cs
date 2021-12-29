28e8e599a67ae028c5f980f6325cdb216272d3e4
celorma <89708923+celorma@users.noreply.github.com>
2021-12-29 16:11:22 -0300

    level 1

--- GitHub prueba/Assets/Scripts/camera.cs
+++ GitHub prueba/Assets/Scripts/camera.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    [SerializeField] GameObject Seguimiento;
+    [SerializeField] Transform follow;
+    [SerializeField] Transform activeRoom;
+
+    public static camera instance;
+
+    [Range(-5,5)]
+    public float minModX, maxModX, minModY, maxModY;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = transform.position;
-        position.x = Seguimiento.transform.position.x;
-        transform.position = position;
+        var minPosY = activeRoom.GetComponent<BoxCollider2D>().bounds.min.y + minModY;
+        var maxPosY = activeRoom.GetComponent<BoxCollider2D>().bounds.max.y + maxModY;
+        var minPosX = activeRoom.GetComponent<BoxCollider2D>().bounds.min.x + minModX;
+        var maxPosX = activeRoom.GetComponent<BoxCollider2D>().bounds.max.x + maxModX;
+
+        Vector3 clampedPos = new Vector3(
+            Mathf.Clamp(follow.position.x, minPosX, maxPosX),
+            Mathf.Clamp(follow.position.y, minPosY, maxPosY),
+            Mathf.Clamp(follow.position.z, -20f, -20f)
+            );
+
+        transform.position = new Vector3(clampedPos.x, clampedPos.y, clampedPos.z);
     }
 }
